beb2ba44290d2603c90c6019a0379d8ff7e3d165
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-04-19 09:57:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-//your code here
+// const btn = document.getElementById('app');
+const swap = document.getElementById('swap');
+const buttonDay = document.querySelector('.button_day');
+
+function swapTheme() {
+	swap.addEventListener('click', function() {
+		buttonDay.style.color = 'blue';
+	});
+};
 
